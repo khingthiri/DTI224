5d3761354069dbe7b8b0dd51322a2647801e2785
Nan Sanda Htun <hteeclaybaw@gmail.com>
2026-08-23 19:56:53 +0700

    Add service worker for caching assets

---
+++ service-worker.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'dti224-cache-v1';
+const urlsToCache = [
+  '/DTI224/',
+  '/DTI224/index.html',
+  '/DTI224/converter.html',
+  '/DTI224/grade.html',
+  '/DTI224/change.html',
+  '/DTI224/distance.html',
+  '/DTI224/hobby.html'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
